6152d42c0e4d68a6ee2c89fc04b1bebf5c6229e3
ykwizera <144004543+ykwizera@users.noreply.github.com>
2024-05-16 12:09:45 +0200

    updated file

--- models/base_model.py
+++ models/base_model.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     print("JSON of my_model:")
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-                                        my_model_json[key]))
+                                    my_model_json[key]))
 
     print("--")
     my_new_model = BaseModel(**my_model_json)
